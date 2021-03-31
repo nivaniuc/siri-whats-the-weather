d8192c3aab38708f0c9381a1838738c4b5560461
nivaniuc <nivaniucc@gmail.com>
2021-03-30 23:39:18 -0400

    Cleaned up JS and did the finishing touches on CSS (it wasnt a lot)

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,18 +1,17 @@
 
-// variable declaration
-var searchCity = $("#search-city");
-var searchButton = $("#search-button");
-var clearButton = $("#clear-history");
-var currentCity = $("#current-city");
+//VARIABLES
 var currentTemperature = $("#temperature");
 var currentHumidty= $("#humidity");
 var currentWSpeed=$("#wind-speed");
 var currentUvindex= $("#uv-index");
+var searchButton = $("#search-button");
+var currentCity = $("#current-city");
+var currentCity = $("#current-city");
+var searchCity = $("#search-city");
 var sCity=[];
 
-//Set up the API key
+
 var APIKey="a0aca8a89948154a4182dcecc780b513";
-// Display the curent and future weather to the user after grabing the city form the input text box.
 function displayWeather(event){
     event.preventDefault();
     if(searchCity.val().trim()!==""){
@@ -21,31 +20,31 @@ function displayWeather(event){
     }
 }
 
-//Click Handlers
+//CLICK HANDLER
 $("#search-button").on("click",displayWeather);
-$(document).on("click",invokePastSearch);
-$(window).on("load",loadlastCity);
-$("#clear-history").on("click",clearHistory);
 
-// Here we create the AJAX call
+//CALL FUNCTION
 function currentWeather(city){
-    // Here we build the URL so we can get a data from server side.
+    
     var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
     $.ajax({
         url:queryURL,
         method:"GET",
     }).then(function(response){
 
+        //TEMPERATURE
         var tempF = (response.main.temp - 273.15) * 1.80 + 32;
         $(currentTemperature).html((tempF).toFixed(2)+"&#8457");
         
+        //HUMIDITY
         $(currentHumidty).html(response.main.humidity+"%");
-        //Display Wind speed and convert to MPH
+
+        //WIND SPEED DISPLAY ((and conversion))
         var ws=response.wind.speed;
         var windsmph=(ws*2.237).toFixed(1);
         $(currentWSpeed).html(windsmph+"MPH");
-        // Display UVIndex.
-        //By Geographic coordinates method and using appid and coordinates as a parameter we are going build our uv query url inside the function below.
+        
+        //UV INDEX
         UVIndex(response.coord.lon,response.coord.lat);
         forecast(response.id);
         if(response.cod==200){
@@ -70,9 +69,8 @@ function currentWeather(city){
     });
 }
 
-    // This function returns the UVIindex response.
+// This returns the UV Index....I got stuck on it earlier...
 function UVIndex(ln,lt){
-    //lets build the url for uvindex.
     var uvqURL="https://api.openweathermap.org/data/2.5/uvi?appid="+ APIKey+"&lat="+lt+"&lon="+ln;
     $.ajax({
             url:uvqURL,
@@ -82,7 +80,7 @@ function UVIndex(ln,lt){
             });
 }
     
-// Here we display the 5 days forecast for the current city.
+//5-DAY FORCAST
 function forecast(cityid){
     var dayover= false;
     var queryforcastURL="https://api.openweathermap.org/data/2.5/forecast?id="+cityid+"&appid="+APIKey;
