2f6d9419d46cd6e4061cc477fbe9e8b3607cf37d
nivaniuc <nivaniucc@gmail.com>
2021-03-30 23:17:40 -0400

    Cleaned up HTML (it was bugging me) and finished 5-day forecast. Cant get images to load for some reason.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -86,4 +86,21 @@ function UVIndex(ln,lt){
 function forecast(cityid){
     var dayover= false;
     var queryforcastURL="https://api.openweathermap.org/data/2.5/forecast?id="+cityid+"&appid="+APIKey;
-
+    $.ajax({
+        url:queryforcastURL,
+        method:"GET"
+    }).then(function(response){
+        
+        for (i=0;i<5;i++){
+            var date= new Date((response.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
+            var tempK= response.list[((i+1)*8)-1].main.temp;
+            var tempF=(((tempK-273.5)*1.80)+32).toFixed(2);
+            var humidity= response.list[((i+1)*8)-1].main.humidity;
+        
+            $("#fDate"+i).html(date);
+            $("#fTemp"+i).html(tempF+"&#8457");
+            $("#fHumidity"+i).html(humidity+"%");
+        }
+        
+    });
+}
