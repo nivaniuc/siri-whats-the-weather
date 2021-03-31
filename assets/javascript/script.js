19b350ee7b49fc656ddf7a92c9d745c719425fb9
nivaniuc <nivaniucc@gmail.com>
2021-03-30 21:06:19 -0400

    Added variables and tried to connect the API to JS. Nothing appears to show up yet.

---
+++ assets/javascript/script.js
@@ -0,0 +1,24 @@
+
+//Declare a variable to store the searched city
+var city="";
+// variable declaration
+var searchCity = $("#search-city");
+var searchButton = $("#search-button");
+var clearButton = $("#clear-history");
+var currentCity = $("#current-city");
+var currentTemperature = $("#temperature");
+var currentHumidty= $("#humidity");
+var currentWSpeed=$("#wind-speed");
+var currentUvindex= $("#uv-index");
+var sCity=[];
+
+//Set up the API key
+var APIKey="a0aca8a89948154a4182dcecc780b513";
+// Display the curent and future weather to the user after grabing the city form the input text box.
+function displayWeather(event){
+    event.preventDefault();
+    if(searchCity.val().trim()!==""){
+        city=searchCity.val().trim();
+        currentWeather(city);
+    }
+}
